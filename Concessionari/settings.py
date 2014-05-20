10e7f7ae0558ac2e677801b9f1fa8f6c9b699c45
dcasajus <deniscasajus@gmail.com>
2014-05-20 17:05:47 +0200

    Finalitzada

--- Concessionari/settings.py
+++ Concessionari/settings.py
@@ -65,7 +65,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/joanmarc/djangostack-1.5.5-2/Concessionari/iConcessionari/static'
+STATIC_ROOT = 'https://github.com/gbarbosa4/Concessionari/tree/master/iConcessionari/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
